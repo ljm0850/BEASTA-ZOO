d83065b2d1fe01740eac6f038297e30f4966d796
joe <gosmf12@naver.com>
2022-09-13 17:08:05 +0900

    S07P22C108-127 뽑기 : 뽑기 후 nft db 저장 api

---
+++ backend/JAV/src/main/java/com/nft/jav/controller/DrawController.java
@@ -0,0 +1,33 @@
+package com.nft.jav.controller;
+
+import com.nft.jav.data.dto.DrawReqDto;
+import com.nft.jav.service.DrawService;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/draw")
+public class DrawController {
+
+    private final Logger logger = LoggerFactory.getLogger(DrawController.class);
+
+    private final DrawService drawService;
+
+    @PostMapping("")
+    public ResponseEntity<String> saveDrawNft(@RequestBody DrawReqDto drawReqDto){
+        logger.info("drawSaveNft- 호출");
+        if(drawService.saveDrawNft(drawReqDto))
+            return new ResponseEntity<>("Success", HttpStatus.OK);
+        return new ResponseEntity<>("Fail",HttpStatus.BAD_REQUEST);
+    }
+
+
+}
