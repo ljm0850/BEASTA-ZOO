7b67d6d87c6f91786d396add21dda2e7b06aca66
김지호 <gosmf12@naver.com>
2022-09-20 20:40:15 +0900

    fix/도감-kjh 유저 도감 수정

--- backend/JAV/src/main/java/com/nft/jav/service/CombiServiceImpl.java
+++ backend/JAV/src/main/java/com/nft/jav/service/CombiServiceImpl.java
@@ -15,6 +15,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
+
 @Service
 @Transactional
 @RequiredArgsConstructor
@@ -36,6 +38,8 @@ public class CombiServiceImpl implements CombiService{
         NFT savedNFT = nftRepository.save(NFT.builder()
                 .user(targetUser)
                 .nft_address(combiReqDto.getNft_address())
+                .jav_code(combiReqDto.getJav_code())
+                .img_address(combiReqDto.getImg_address())
                 .build());
 
         // 조합으로 쓰인 NFT 삭제
@@ -51,23 +55,28 @@ public class CombiServiceImpl implements CombiService{
         ServiceCollection serviceTarget = serviceCollectionRepository.findByJav_code(combiReqDto.getJav_code());
 
         long userCount = targetUser.getFirst_discover_count();
-        if(userCount==0){
+        int serviceCount = serviceTarget.getDiscover_user_count();
+
+        if(serviceCount==0){
 
             // user정보에 최초 발견 추가
             targetUser.updateFirstDiscoverCount(userCount+1);
 
             // serviceTarget에 최초 발견자 추가
             serviceTarget.updateUser(targetUser);
+
+            // 최초 발견시간 저장
+            serviceTarget.updateDiscoverTime(LocalDateTime.now());
         }
 
         // 전체 도감 발견자 수 추가
-        int serviceCount = serviceTarget.getDiscover_user_count();
         serviceTarget.updateDiscoverUserCount(serviceCount+1);
 
         // 유저 도감에 추가
         UserCollection userCollection = UserCollection.builder()
                 .user(targetUser)
                 .nft_address(combiReqDto.getNft_address())
+                .img_address(savedNFT.getImg_address())
                 .jav(serviceTarget)
                 .build();
 
