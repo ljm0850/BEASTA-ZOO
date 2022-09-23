6415c666b13d034d51938d0d3f731d8568039e0c
김세진 <rlatpwls15@naver.com>
2022-09-23 17:07:38 +0900

    S07P22C108-187 feat: truffle 설정 변경

--- smart-contracts/migrations/1_initial_migration.js
+++ smart-contracts/migrations/1_initial_migration.js
@@ -1,19 +1,19 @@
 const Migrations = artifacts.require("Migrations");
-const SsafyToken = artifacts.require("SsafyToken");
-const SsafyNFT = artifacts.require("SsafyNFT");
+const JavToken = artifacts.require("JavToken");
+const JAV_NFT = artifacts.require("JAV_NFT");
 const SaleFactory = artifacts.require("SaleFactory");
 
 /**
  * PJT Ⅰ/Ⅲ - 시나리오 테스트
- * @dev 
- * 올바른 테스트를 위해 
+ * @dev
+ * 올바른 테스트를 위해
  * PJT Ⅰ - SsafyNFT
  * PJT Ⅲ - SsafyNFT, SsafyToken, SaleFactory
- * 가 배포되어야 합니다. 
+ * 가 배포되어야 합니다.
  */
 module.exports = function (deployer) {
-  deployer.deploy(Migrations); 
-  deployer.deploy(SsafyNFT); 
-  deployer.deploy(SsafyToken, "SSAFY", "SSF", 0);
-  deployer.deploy(SaleFactory);
+  deployer.deploy(Migrations);
+  deployer.deploy(JAV_NFT);
+  await const NFTAddress = deployer.deploy(JavToken, "JavJong", "JAV", 0);
+  deployer.deploy(SaleFactory, NFTAddress);
 };
