6a8d58833cecd169efaa7f4e780eddb1d1b0d129
kimsejin <rlatpwls15@naver.com>
2022-09-26 11:28:20 +0900

    S07P22C108-191 fix: JAVToken mint 권한 해제

--- smart-contracts/contracts/JavToken.sol
+++ smart-contracts/contracts/JavToken.sol
@@ -12,7 +12,7 @@ contract JavToken is ERC20, Ownable{
     
     constructor(string memory name, string memory symbol, uint8 decimal) ERC20(name, symbol, decimal) {}
     
-    function mint(uint256 amount) public onlyOwner{
+    function mint(uint256 amount) public {
         _mint(_msgSender(), amount);
     }
     
