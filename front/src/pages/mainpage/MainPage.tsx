0eba7e7267f8bac0b1ec09529ad2ae07f5eca8df
HeoJaeyoung <gj3034@naver.com>
2022-09-26 09:22:45 +0900

    style: css 버그 수정

--- front/src/pages/mainpage/MainPage.tsx
+++ front/src/pages/mainpage/MainPage.tsx
@@ -1,12 +1,30 @@
-import { useNavigate } from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 
 import Login from "../../layouts/Login"
 import Draw from "../Draw";
+import { useEffect, useState } from "react";
 
 const MainPage = () => {
   const navigate = useNavigate();
+
+  // 임시 프로필 이동 버튼 
+  const [ account, setAccount ] = useState("")
+  
+  const getAccount = async () => {
+    const accounts = await window.ethereum.request({ method: "eth_accounts" });
+    setAccount(accounts[0]);
+  };
+
+  useEffect(() => {
+    getAccount()
+  }, [])
+
+  const profileMoveHandler = () => {
+    navigate(`user/${account}`)
+  }
+
 
   return (
     <div style={{height: "100vh", display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
@@ -16,6 +34,7 @@ const MainPage = () => {
         </Button>
         <Login></Login>
         <Draw />
+        { sessionStorage.getItem("isLogined") && <Button variant="contained" onClick={profileMoveHandler}>마이프로필로 이동</Button>}
       </Box>
     </div>
   );
