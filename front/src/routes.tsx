07d12e593fdfbc46249459958580e7262eb127e9
YunH <yunh04131@gmail.com>
2022-09-26 10:00:15 +0900

    [S07P22C108-206] feat: 마켓 판매 틀 잡기

--- front/src/routes.tsx
+++ front/src/routes.tsx
@@ -5,6 +5,7 @@ import MainLayout from "./layouts/main";
 import MainPage from "./pages/mainPage/MainPage";
 import ItemDraw from "./pages/market/ItemDraw";
 import Items from "./pages/market/Items";
+import SaleRegistration from "./pages/market/SaleRegistration";
 
 export default function Router() {
   return useRoutes([
@@ -23,6 +24,7 @@ export default function Router() {
       children: [
         { path: "", element: <Items /> },
         { path: "draw", element: <ItemDraw /> },
+        { path: "register", element: <SaleRegistration /> },
       ],
     },
   ]);
