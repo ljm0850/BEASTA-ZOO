b2fd3bf3540442eb586747273a8538ab1460bd42
joe <gosmf12@naver.com>
2022-09-28 10:05:18 +0900

    S07P22C108-260 : 게시물, 댓글 지움

--- backend/JAV/src/main/java/com/nft/jav/controller/CommunityController.java
+++ backend/JAV/src/main/java/com/nft/jav/controller/CommunityController.java
@@ -20,52 +20,52 @@ public class CommunityController {
 
     private final CommunityService communityService;
 
-    @GetMapping
-    public ResponseEntity<List<CommunityResDto>> communityList() {
-        logger.info("commnunityList - 호출");
-        return new ResponseEntity<>(communityService.communityList(), HttpStatus.OK);
-    }
-
-    @GetMapping("/notice")
-    public ResponseEntity<List<CommunityResDto>> noticeList() {
-        logger.info("noticeList - 호출");
-        return new ResponseEntity<>(communityService.noticeList(), HttpStatus.OK);
-    }
-
-    @GetMapping("/{community_id}")
-    public ResponseEntity<CommunityResDto> detailCommunity(@PathVariable long community_id) {
-        logger.info("detailCommunity - 호출");
-        return new ResponseEntity<>(communityService.detailCommunity(community_id),HttpStatus.OK);
-    }
-
-    @PostMapping
-    public ResponseEntity<String> saveCommunity(@RequestBody CommunityReqDto communityDto) {
-        logger.info("saveCommunity - 호출");
-
-        if(communityService.saveCommunity(communityDto))
-            return new ResponseEntity<>("Success",HttpStatus.OK);
-        return new ResponseEntity<>("Fail",HttpStatus.BAD_REQUEST);
-    }
-
-    @PutMapping
-    public ResponseEntity<String> updateCommunity(@RequestBody CommunityModiReqDto communityModiReqDto){
-        logger.info("updateCommunity - 호출");
-
-         if(communityService.updateCommunity(communityModiReqDto)){
-             return new ResponseEntity<>("Success", HttpStatus.OK);
-         }
-        return new ResponseEntity<>("Fail",HttpStatus.BAD_REQUEST);
-    }
-
-    @DeleteMapping("/{community_id}")
-    public ResponseEntity<String> deleteCommunity(@PathVariable long community_id){
-        logger.info("deleteCommunity - 호출");
-
-        if(communityService.deleteCommunity(community_id)){
-            return new ResponseEntity<>("Success", HttpStatus.OK);
-        }
-        return new ResponseEntity<>("Fail",HttpStatus.BAD_REQUEST);
-    }
+//    @GetMapping
+//    public ResponseEntity<List<CommunityResDto>> communityList() {
+//        logger.info("commnunityList - 호출");
+//        return new ResponseEntity<>(communityService.communityList(), HttpStatus.OK);
+//    }
+//
+//    @GetMapping("/notice")
+//    public ResponseEntity<List<CommunityResDto>> noticeList() {
+//        logger.info("noticeList - 호출");
+//        return new ResponseEntity<>(communityService.noticeList(), HttpStatus.OK);
+//    }
+//
+//    @GetMapping("/{community_id}")
+//    public ResponseEntity<CommunityResDto> detailCommunity(@PathVariable long community_id) {
+//        logger.info("detailCommunity - 호출");
+//        return new ResponseEntity<>(communityService.detailCommunity(community_id),HttpStatus.OK);
+//    }
+//
+//    @PostMapping
+//    public ResponseEntity<String> saveCommunity(@RequestBody CommunityReqDto communityDto) {
+//        logger.info("saveCommunity - 호출");
+//
+//        if(communityService.saveCommunity(communityDto))
+//            return new ResponseEntity<>("Success",HttpStatus.OK);
+//        return new ResponseEntity<>("Fail",HttpStatus.BAD_REQUEST);
+//    }
+//
+//    @PutMapping
+//    public ResponseEntity<String> updateCommunity(@RequestBody CommunityModiReqDto communityModiReqDto){
+//        logger.info("updateCommunity - 호출");
+//
+//         if(communityService.updateCommunity(communityModiReqDto)){
+//             return new ResponseEntity<>("Success", HttpStatus.OK);
+//         }
+//        return new ResponseEntity<>("Fail",HttpStatus.BAD_REQUEST);
+//    }
+//
+//    @DeleteMapping("/{community_id}")
+//    public ResponseEntity<String> deleteCommunity(@PathVariable long community_id){
+//        logger.info("deleteCommunity - 호출");
+//
+//        if(communityService.deleteCommunity(community_id)){
+//            return new ResponseEntity<>("Success", HttpStatus.OK);
+//        }
+//        return new ResponseEntity<>("Fail",HttpStatus.BAD_REQUEST);
+//    }
 
     @GetMapping("/rank")
     public ResponseEntity<List<RankResDto>> rankUser(){
