894b3e27c6f70ff20c163566f6c8e83636b5e3bf
YunH <yunh04131@gmail.com>
2022-10-05 14:08:50 +0900

    [No Issue] hotfix : FAQ 머지하며 생긴 오류 수정

--- front/src/layouts/main/FAQ.tsx
+++ front/src/layouts/main/FAQ.tsx
@@ -9,7 +9,7 @@ import Qmark from "../../image/mainPage/Qmark.png";
 
 export default function FAQ() {
   return (
-    <div style={{margin: "90px 0 90px 0"}}>
+    <div style={{ margin: "90px 0 90px 0" }}>
       <p className={styles.FAQTitle}>FAQ</p>
       <Accordion className={styles.accordion}>
         <AccordionSummary
@@ -19,20 +19,17 @@ export default function FAQ() {
           sx={{ height: "5rem" }}
         >
           <img src={Qmark} alt="" />
-<<<<<<< HEAD
-          <Typography sx={{ marginLeft: "1rem", display: "flex", justifyContent: "start", alignItems: "center", fontWeight: "700"}}>NFT란 무엇인가요?</Typography>
-=======
           <Typography
             sx={{
               marginLeft: "1rem",
               display: "flex",
               justifyContent: "start",
               alignItems: "center",
+              fontWeight: "700",
             }}
           >
             NFT란 무엇인가요?
           </Typography>
->>>>>>> c4a1ed89b98deb2f95e1aea43528262e8dd4bfbb
         </AccordionSummary>
         <AccordionDetails>
           <Typography>
@@ -49,20 +46,17 @@ export default function FAQ() {
           sx={{ height: "5rem" }}
         >
           <img src={Qmark} alt="" />
-<<<<<<< HEAD
-          <Typography sx={{ marginLeft: "1rem", display: "flex", justifyContent: "start", alignItems: "center", fontWeight: "700"}}>JAV NFT는 어떤 블록체인을 사용하나요?</Typography>
-=======
           <Typography
             sx={{
               marginLeft: "1rem",
               display: "flex",
               justifyContent: "start",
               alignItems: "center",
+              fontWeight: "700",
             }}
           >
             JAV NFT는 어떤 블록체인을 사용하나요?
           </Typography>
->>>>>>> c4a1ed89b98deb2f95e1aea43528262e8dd4bfbb
         </AccordionSummary>
         <AccordionDetails>
           <Typography>
@@ -79,20 +73,17 @@ export default function FAQ() {
           sx={{ height: "5rem" }}
         >
           <img src={Qmark} alt="" />
-<<<<<<< HEAD
-          <Typography sx={{ marginLeft: "1rem", display: "flex", justifyContent: "start", alignItems: "center", fontWeight: "700"}}>JAV NFT는 어떤 프로젝트인가요?</Typography>
-=======
           <Typography
             sx={{
               marginLeft: "1rem",
               display: "flex",
               justifyContent: "start",
               alignItems: "center",
+              fontWeight: "700",
             }}
           >
             JAV NFT는 어떤 프로젝트인가요?
           </Typography>
->>>>>>> c4a1ed89b98deb2f95e1aea43528262e8dd4bfbb
         </AccordionSummary>
         <AccordionDetails>
           <Typography>
@@ -109,20 +100,17 @@ export default function FAQ() {
           sx={{ height: "5rem" }}
         >
           <img src={Qmark} alt="" />
-<<<<<<< HEAD
-          <Typography sx={{ marginLeft: "1rem", display: "flex", justifyContent: "start", alignItems: "center", fontWeight: "700"}}>JAV는 무엇인가요?</Typography>
-=======
           <Typography
             sx={{
               marginLeft: "1rem",
               display: "flex",
               justifyContent: "start",
               alignItems: "center",
+              fontWeight: "700",
             }}
           >
             JAV는 무엇인가요?
           </Typography>
->>>>>>> c4a1ed89b98deb2f95e1aea43528262e8dd4bfbb
         </AccordionSummary>
         <AccordionDetails>
           <Typography>
